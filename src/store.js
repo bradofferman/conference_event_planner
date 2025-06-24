83a345786b86d7b24e31541685d34f813aa74bd9
bradofferman <164579005+bradofferman@users.noreply.github.com>
2025-06-24 16:31:26 -0700

    working changes

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
+import toggleMealSelection from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: toggleMealSelection,
   },
 });
